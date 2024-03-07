28661f026f01049042619e9dc3d81dfcfbad6cee
mcarena <mcarena.quijada@outlook.com>
2024-03-06 23:17:41 -0300

    Se incorpora el punto 2 donde se rescatan los datos y se imprime en forma de lista el title y el body

--- desafioI.js
+++ desafioI.js
@@ -1,13 +1,24 @@
-
-// FUNCION ASINCRONICA
+const displayPosts = (posts) => {
+    const postListElement = document.getElementById('post-data');
+    postListElement.innerHTML = ''; 
+// PUNTO 2 
+    posts.forEach((post) => {
+        const listItem = document.createElement('li');
+        listItem.textContent = `${post.title}`;
+        listItem.textContent = `${post.body}`;
+        postListElement.appendChild(listItem);
+    });
+};
+// PUNTO 1 FUNCION ASINCRONICA
 const getPost = async () => {
     const url = 'https://jsonplaceholder.typicode.com/posts';
     
+
+    //PUNTO 3
     try {
         const response = await fetch(url);
-
+        // REVELA EL ESTADO DE ERROR
         if (!response.ok) {
-            // REVELA EL ESTADO DE ERROR
             throw new Error(`Error al obtener los datos. Código de estado: ${response.status}`);
         }
 
