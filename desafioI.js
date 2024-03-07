3332a6081ab37a058aa8869014516391c3bfde80
mcarena <mcarena.quijada@outlook.com>
2024-03-06 22:26:40 -0300

    Se hacen cambios en el codigo como el if que esta dentro del try por un error mostrando el estado

--- desafioI.js
+++ desafioI.js
@@ -1,3 +1,4 @@
+
 // FUNCION ASINCRONICA
 const getPost = async () => {
     const url = 'https://jsonplaceholder.typicode.com/posts';
@@ -6,18 +7,13 @@ const getPost = async () => {
         const response = await fetch(url);
 
         if (!response.ok) {
+            // REVELA EL ESTADO DE ERROR
             throw new Error(`Error al obtener los datos. Código de estado: ${response.status}`);
         }
 
         const datos = await response.json();
-
-        datos.slice(0, 20).forEach((dato, index) => {
-            console.log(`Dato ${index + 1}:`, dato); 
-        });
-
+        displayPosts(datos);
         
-         console.log("Informacion a procesar: ", datos);
-
     } catch (error) {
         alert(error.message);
     }
